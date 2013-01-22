275220517113babdc23aa6e640ff0cb5bcf507fb
Robert Warner <radicalphoenix@gmail.com>
2013-01-21 17:26:18 -0800

    latest stuffs

--- tracker.py
+++ tracker.py
@@ -1,7 +1,10 @@
-from wsgiref.simple_server import make_server
 import datetime
 import calendar
+import pystache
 
+
+def chunk_list(lst, size):
+    return [lst[i:i + size] for i in range(0, len(lst), size)]
 
 data = {
     '2013-01-01': 'good',
@@ -14,17 +17,6 @@ data = {
     '2013-01-08': 'good'
 }
 
-# data = {
-#     '2013-01-01': 'good',
-#     '2013-01-02': 'good',
-#     '2013-01-03': 'great',
-#     '2013-01-04': 'good',
-#     '2013-01-05': 'bad',
-#     '2013-01-06': 'bad',
-#     '2013-01-07': 'terrible'
-# }
-
-
 ratings = {
     'great': 2,
     'good': 1,
@@ -32,172 +24,116 @@ ratings = {
     'terrible': -1
 }
 
+thresholds = {
+    'great': 2 * ratings['great'] + 5 * ratings['good'],
+    'good': 7 * ratings['good'],
+    'bad': 5 * ratings['good'],
+    'terrible': None
+}
+
+
+def get_day_key(day):
+    return day.strftime('%Y-%m-%d')
+
+
+def get_rating_for_day(day):
+    return data.get(get_day_key(day), None)
+
+
+def set_rating_for_day(day, rating):
+    assert rating in ratings
+    data[get_day_key(day)] = rating
+
+
+def delete_rating_for_day(day):
+    del data[get_day_key(day)]
+
 
 def rate_week(days, today):
-    thresholds = {
-        'great': 2 * ratings['great'] + 5 * ratings['good'],
-        'good': 7 * ratings['good'],
-        'bad': 5 * ratings['good'],
-        'terrible': None
-    }
-
+    normal_points = sum(map(lambda day: ratings.get(get_rating_for_day(day), 0), days))
     grace_points = sum(day.year != today.year for day in days)
 
-    score = get_score_for_week(days) + grace_points
-    for rating, level in thresholds.iteritems():
-        if score > level:
-            week_status = rating
-            break
+    return next(rating for rating, level in thresholds.iteritems() if normal_points + grace_points > level)
+
+
+def get_classes_for_date(day, today):
+    statuses = []
+
+    if day.year != today.year:
+        statuses.append('not-this-year')
+    elif day == today:
+        statuses.append('today')
+    elif day > today:
+        statuses.append('future')
+
+    status = get_rating_for_day(day)
+    if status:
+        statuses.append(status)
+    else:
+        statuses.append('nodata')
+
+    return ' '.join(statuses)
+
+
+def get_week_status(week, today):
+    if all(day > today for day in week):
+        week_status = 'blank'
+    elif any(day > today for day in week):
+        week_status = 'pending'
+    else:
+        week_status = rate_week(week, today)
 
     return week_status
 
 
-def get_score_for_week(days):
-    return sum(ratings.get(get_status_for_date(day), 0) for day in days)
+def week_days_for_year(year, start_day):
+    cal = calendar.Calendar(start_day)
+    return [day
+            for month in range(1, 13)
+            for day in cal.itermonthdates(year, month)]
 
 
-def get_classes_for_date(date, today):
-    statii = []
+def get_weeks(today=datetime.date.today(), start_day=6):
+    weeks = chunk_list(week_days_for_year(today.year, start_day), 7)
 
-    if date.year != today.year:
-        statii.append('blank')
+    weeks_data = []
+    for week in weeks:
+        week_data = {
+            'days': [{'classes': get_classes_for_date(day, today)} for day in week],
+            'status': get_week_status(week, today)
+        }
 
-    if date == today:
-        statii.append('today')
-    elif date > today:
-        statii.append('future')
+        weeks_data.append(week_data)
 
-    status = get_status_for_date(date)
-    if status:
-        statii.append(status)
-    else:
-        statii.append('nodata')
-
-    return ' '.join(statii)
-
-
-def get_status_for_date(date):
-    day_key = date.strftime('%Y-%m-%d')
-    return data.get(day_key, None)
+    return weeks_data
 
 
 def application(env, start_response):
-    content = '''
-    <html>
-    <head>
-    <style type="text/css">
-        ul.calender-grid {
-            width: 520px;
-            padding: 0px;
-            margin: 0px;
-        }
-        ul.calendar-grid .week {
-            display: inline;
-            list-style-type: none;
-            float: left;
-        }
-
-        ul.calendar-grid .week ul {
-            padding: 0px;
-        }
-
-        .square  {
-            border: 1px solid white;
-            display: list-item;
-            width: 10px;
-            height: 10px;
-            list-style-type: none;
-        }
-
-        .nodata {
-            background: #CCC;
-        }
-
-        .future {
-            background: #EEE;
-        }
-
-        .great {
-            background: #FFD700;
-        }
-
-        .good {
-            background: #33DD77;
-        }
-
-        .bad {
-            background: #BB4433;
-        }
-
-        .terrible {
-            background: #000;
-        }
-
-        .blank {
-            background: none;
-        }
-
-        .today {
-            border: 1px dotted blue;
-        }
-
-        .week-summary {
-            margin-top: 10px;
-        }
-
-        .week-summary.pending {
-            border: 1px dotted blue;
-        }
-
-    </style>
-    </head>
-    <body>
-    '''
-
-    today = datetime.date.today()
-
-    cal = calendar.Calendar(6)
-
-    days = [day
-            for month in range(1, 13)
-            for day in cal.itermonthdates(today.year, month)]
-
-    weeks = [days[i:i + 7] for i in range(0, len(days), 7)]
-
-    content += '<ul class="calendar-grid">'
-    for week in weeks:
-        content += '<li class="week"><ul>'
-
-        for day in week:
-            content += '<li class="square {classes}"></li>'.format(week=week, day=day, classes=get_classes_for_date(day, today))
-
-        if all(day > today for day in week):
-            week_status = 'blank'
-        elif any(day > today for day in week):
-            week_status = 'pending'
-
-        else:
-            week_status = rate_week(week, today)
-
-        content += '<li class="square week-summary {week_status}"></li>'.format(week_status=week_status)
-
-        content += '</ul></li>'
-
-    content += '</ul>'
-
-    content += '''
-    </body>
-    </html>
-    '''
+    with open('grid_tracker.mustache') as template:
+        content = pystache.render(
+            template.read(),
+            {'weeks': get_weeks()}
+        )
 
     status = '200 OK'
     headers = [('Content-type', 'text/html')]
-
     start_response(status, headers)
 
     return [content]
 
 
 if __name__ == '__main__':
-    httpd = make_server('', 8000, application)
+    from wsgiref.simple_server import make_server
+
+    # simplewsgi is failing on non-str content
+    # so we'll use a simple wrapper to force it to str
+    # but not affect other wsgi servers
+
+    def force_str_content(application):
+        def wrapped(env, start_response):
+            return map(str, application(env, start_response))
+
+        return wrapped
+
+    httpd = make_server('', 8888, force_str_content(application))
     httpd.serve_forever()
